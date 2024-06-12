c71a89a2a44d2d184744dc51b81ae50577255d9d
Robson Kretzmann da Silva <robsonkretzmann@hotmail.com>
2024-06-12 20:10:24 -0300

    initialCommit

---
+++ src/main/kotlin/com/websocketAPI/WebsocketApiApplication.kt
@@ -0,0 +1,11 @@
+package com.websocketAPI
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebsocketApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<WebsocketApiApplication>(*args)
+}
